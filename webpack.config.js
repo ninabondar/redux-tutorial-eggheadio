18d73b92a6322d16e2e9c22c9f3ff0179314f7ef
ninabondar <bondar_n@ucu.edu.ua>
2018-03-03 14:37:12 +0200

    create first toggleTodo implementation Lesson 10

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: ["./src/js/CounterReducer.js"],
+    entry: ["./src/js/ToDo.js"],
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
