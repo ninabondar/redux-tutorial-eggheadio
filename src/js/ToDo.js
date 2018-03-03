bbdd8a4baec1838fec21c335bf23cf02aa93890f
ninabondar <bondar_n@ucu.edu.ua>
2018-03-03 15:04:46 +0200

    create a reducer for todo list Lesson 11

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -8,11 +8,45 @@ const toggleToDo = (todoItem) => {
         completed: !todoItem.completed
     };
 };
-
-
+//reducer for todoList: a pure func that describes logics of changing the state( === changing todos) of this app
+const todos = (state = [], action) => {
+  switch (action.type){
+      case 'ADD_TODO':
+          return [...state, {
+              id: action.id,
+              text: action.text,
+              completed: false
+              }
+          ];
+      default:
+          return state;
+  }
+};
 
 //---------------------------------------------TESTS-----------------------------------------------
 
+const testAddTodo = () => {
+  let stateBefore  = [];
+  deepFreeze(stateBefore);
+  const action = {
+      type: "ADD_TODO",
+      id: 0,
+      text: 'Learn Redux'
+  };
+  deepFreeze(action);
+  const stateAfter = [
+      {
+      id: 0,
+      text: "Learn Redux",
+      completed: false
+      }
+  ];
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+  console.log('passed testAddTodo')
+};
+
+testAddTodo();
 
 const testToggleToDo = () => {
     let todoBefore = {
