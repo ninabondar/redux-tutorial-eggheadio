35321aef96451b13d32f50c8771950cf64aadee1
ninabondar <bondar_n@ucu.edu.ua>
2018-03-04 21:03:34 +0200

    implement composition of reducers Lesson 14

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -1,21 +1,9 @@
 import deepFreeze from 'deep-freeze';
 import expect from 'expect';
+import Redux from 'redux';
 
-//reducer for todoList: a pure func that describes logics of changing the state( === changing todos) of this app
-const todos = (state = [], action) => {
-  switch (action.type){
-      case 'ADD_TODO':
-          return [...state, todo(undefined, action)
-          ];
-      case "TOGGLE_TODO":
-          return state.map(t => todo(t, action));
-      default:
-          return state;
-  }
-};
 
 //logics of updating a todoItem must be saved in another place(function), not right in the reducer:
-
 const todo = (state, action) => {
     switch(action.type){
         case "ADD_TODO":
@@ -37,6 +25,45 @@ const todo = (state, action) => {
             return state;
     }
 };
+
+//reducer for todoList: a pure func that describes logics of changing the state( === changing todos) of this app
+const todos = (state = [], action) => {
+  switch (action.type){
+      case 'ADD_TODO':
+          return [...state, todo(undefined, action)
+          ];
+      case "TOGGLE_TODO":
+          return state.map(t => todo(t, action));
+      default:
+          return state;
+  }
+};
+
+const visibilityFilter = (state="SHOW_ALL", action) => {
+    switch(action.type){
+        case "SET_VISIBILITY_FILTER":
+            return action.filter;
+        default:
+            return state;
+    }
+};
+//this is the reducer that will show the todos depending on the visibility filter set
+const todoApp = (state={}, action) => {
+    return {
+        todos: todos(state.todos, action),
+        visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+    }
+};
+
+const { createStore } =  Redux;
+const store = createStore(todoApp());
+store.dispatch({
+    type: "ADD_TODO",
+    id: 0,
+    text: "LEARN Redux"
+});
+
+console.log('current state: ' + store.getState());
 //---------------------------------------------TESTS-----------------------------------------------
 
 const testAddTodo = () => {
